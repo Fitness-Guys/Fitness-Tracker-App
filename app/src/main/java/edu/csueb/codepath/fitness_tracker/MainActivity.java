1b279ee56c736d2a215a4adfa66b7ecc8e866b47
NeilSingh21 <suritaneilsahota1@gmail.com>
2021-03-24 12:51:35 -0700

    Initial commit

---
+++ app/src/main/java/edu/csueb/codepath/fitness_tracker/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.csueb.codepath.fitness_tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
