1d6ed557d60ba1ad2a35883acf1848323b85c6a0
NeilSingh21 <suritaneilsahota1@gmail.com>
2021-03-26 14:33:10 -0700

    Bottom Navigation Menu with Fragments

--- app/src/main/java/edu/csueb/codepath/fitness_tracker/MainActivity.java
+++ app/src/main/java/edu/csueb/codepath/fitness_tracker/MainActivity.java
@@ -1,14 +1,55 @@
 package edu.csueb.codepath.fitness_tracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import edu.csueb.codepath.fitness_tracker.fragments.HomeFragment;
+import edu.csueb.codepath.fitness_tracker.fragments.LearnFragment;
+import edu.csueb.codepath.fitness_tracker.fragments.ProfileFragment;
+import edu.csueb.codepath.fitness_tracker.fragments.TrackFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    final FragmentManager mFragmentManager = getSupportFragmentManager();
+    private BottomNavigationView mBottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.action_learn:
+                        fragment = new LearnFragment();
+                        break;
+                    case R.id.action_track:
+                        fragment = new TrackFragment();
+                        break;
+                    case R.id.action_profile:
+                    default:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+                mFragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        mBottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 }
---
+++ app/src/main/java/edu/csueb/codepath/fitness_tracker/fragments/HomeFragment.java
@@ -0,0 +1,36 @@
+package edu.csueb.codepath.fitness_tracker.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.csueb.codepath.fitness_tracker.R;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    // The onCreateView method is called when Fragment should create its View object hierarchy
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        setHasOptionsMenu(true);
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here. E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
