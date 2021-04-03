5a53d5889b55356b6a70c2cea9122acd2f6f8833
Ing-Ram <chad117gram@gmail.com>
2021-04-03 15:00:16 -0700

    Chad: implemented a LoginActivity page, connected it to a parse backend, and built in an extension for new users to sign up. Will need to work in backend to have users point to workouts.

---
+++ app/src/main/java/edu/csueb/codepath/fitness_tracker/LoginActivity.java
@@ -0,0 +1,105 @@
+package edu.csueb.codepath.fitness_tracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class LoginActivity extends AppCompatActivity {
+    public static final String TAG = "LoginActivity";
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+
+    private EditText etEmailAddress;
+    private EditText etNewWeight;
+    private EditText etNewHeight;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        
+        if(ParseUser.getCurrentUser() != null){
+            goMainActivity();
+        }
+        
+        etUsername =  findViewById(R.id.etUsername);
+        etPassword =  findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnsubmit);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG,"onClick Login button clicked");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username, password);
+            }
+        });
+
+        etNewHeight = findViewById(R.id.etNewHeight);
+        etNewWeight = findViewById(R.id.etNewWeight);
+        etEmailAddress = findViewById(R.id.etEmailAddress);
+        
+    }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "Attempting to log in user" + username);
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e!= null){
+                    Log.e(TAG, "Issue with login", e);
+                    return;
+                }
+                goMainActivity();
+                Toast.makeText(LoginActivity.this, "Success", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void signup(View view){
+        ParseUser user = new ParseUser();
+        user.setPassword(etPassword.getText().toString());
+        user.setUsername(etUsername.getText().toString());
+        user.setEmail(etEmailAddress.getText().toString());
+        /**
+         * will also need to take in:
+         * email
+         * height
+         * weight
+         * date of birth // to increment age
+         * age
+         *
+         *
+         * */
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e!= null){
+                    Toast.makeText(LoginActivity.this, "Sign Up in UNSUCCESSFUL", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, "Sign up Failed!");
+                }else {
+                    Toast.makeText(LoginActivity.this, "Sign up SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                    loginUser(etUsername.toString(), etPassword.toString());
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/edu/csueb/codepath/fitness_tracker/ParseApplication.java
@@ -0,0 +1,23 @@
+package edu.csueb.codepath.fitness_tracker;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+//        ParseObject.registerSubclass((?).class); We will need to connect to the database of workout here.
+
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("mTgNYUC7mlx5G9v1vGQuatFrdiFr6sWGYTV2TnMc")
+                .clientKey("Lldz8npc4JQiFtox5pyYnKwE76V6gxOvet3KU8xO")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
